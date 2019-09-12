61405dc5346f52518d5f9b0ea72ea7572401ab50
geoffrey.kio <geoffrey.kio@NEITD-211214>
2019-09-11 19:12:04 -0500

    new ch

---
+++ classwork/MovieLibraly/itse1430.MoiviesLib.Host/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace itse1430.MoiviesLib.Host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            /// <summary>Represent movie data.</summary>
+            InitializeComponent ();
+            
+        }
+    }
+}
---
+++ classwork/MovieLibraly/itse1430.MoiviesLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("itse1430.MoiviesLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("itse1430.MoiviesLib")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25afa7c9-b506-4ab7-89b6-052a48d7674b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
