dbae6151acf39d6bd916547f18c7901ca85c43dc
geoffrey.kio <geoffrey.kio@NEITD-211214>
2019-10-07 17:31:12 -0500

    test

--- classwork/MovieLibrary/Itse1430.MovieLib.Host/Program.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/Program.cs
@@ -6,7 +6,7 @@ namespace Itse1430.MovieLib.Host
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application .
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main ()
