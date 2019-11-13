4c91a3440cce01e06954c496d3c3561b528018d9
geoffrey.kio <geoffrey.kio@NEITD-211214>
2019-11-13 17:21:49 -0600

    lab 3 finished database

--- classwork/MovieLibrary/Itse1430.MovieLib/IO/FileMovieDatabase.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib/IO/FileMovieDatabase.cs
@@ -15,54 +15,54 @@ namespace Itse1430.MovieLib.IO
         }
 
         protected override Movie AddCore ( Movie movie )
-        {
-            var movies = GetAllCore ().ToList ();
+        {                        
+            var movies = GetAllCore().ToList();
 
             //Figure out highest ID
-            if (movies.Any ())
-                movie.Id = movies.Max (m => m.Id) + 1;
+            if (movies.Any())
+                movie.Id = movies.Max(m => m.Id) + 1;
             else
                 movie.Id = 1;
 
-            movies.Add (movie);
-            SaveMovies (movies);
+            movies.Add(movie);
+            SaveMovies(movies);
 
             return movie;
         }
 
         protected override IEnumerable<Movie> GetAllCore ()
         {
-            if (File.Exists (_filePath))
+            if (File.Exists(_filePath))
             {
-                var lines = File.ReadAllLines (_filePath);
+                var lines = File.ReadAllLines(_filePath);
                 foreach (var line in lines)
                 {
-                    var movie = LoadMovie (line);
+                    var movie = LoadMovie(line);
                     if (movie != null)
                         yield return movie;
                 };
-            };
+            };            
         }
 
         private readonly string _filePath;
 
         protected override Movie GetByNameCore ( string name )
-        {
-            var reader = new StreamReader (_filePath);
+        {            
+            var reader = new StreamReader(_filePath);
             try
             {
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine ();
-                    var movie = LoadMovie (line);
-                    if (String.Compare (movie?.Title, name, true) == 0)
+                    var line = reader.ReadLine();
+                    var movie = LoadMovie(line);
+                    if (String.Compare(movie?.Title, name, true) == 0)
                         return movie;
-                };
+                };                
             } finally
             {
                 //reader.Dispose();
-                reader.Close ();
-            };
+                reader.Close();
+            };           
 
             return null;
         }
@@ -75,16 +75,16 @@ namespace Itse1430.MovieLib.IO
 
             //using (var stream = File.OpenRead(""))
             //using (var reader2 = new StreamReader(stream))            
-
+            
             //Reader way            
-            using (var reader = new StreamReader (_filePath))
+            using (var reader = new StreamReader(_filePath))
             {
                 //try
                 //{
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine ();
-                    var movie = LoadMovie (line);
+                    var line = reader.ReadLine();
+                    var movie = LoadMovie(line);
                     if (movie?.Id ==  id)
                         return movie;
                 };
@@ -99,48 +99,48 @@ namespace Itse1430.MovieLib.IO
 
         protected override void RemoveCore ( int id )
         {
-            var movies = GetAllCore ()
-                                .Where (m => m.Id != id);
+            var movies = GetAllCore()
+                                .Where(m => m.Id != id);
 
-            SaveMovies (movies);
+            SaveMovies(movies);
         }
 
         protected override Movie UpdateCore ( int id, Movie newMovie )
         {
-            var movies = GetAllCore ()
-                                .Where (m => m.Id != id);
+            var movies = GetAllCore()
+                                .Where(m => m.Id != id);
 
             newMovie.Id = id;
-            movies = movies.Union (new[] { newMovie });
+            movies = movies.Union(new[] { newMovie });
 
-            SaveMovies (movies);
+            SaveMovies(movies);
             return newMovie;
         }
 
         private void SaveMovies ( IEnumerable<Movie> items )
         {
-            var lines = items.Select (i => SaveMovie (i)).ToArray ();
-            File.WriteAllLines (_filePath, lines);
+            var lines = items.Select(i => SaveMovie(i)).ToArray();
+            File.WriteAllLines(_filePath, lines);
         }
 
         private Movie LoadMovie ( string line )
         {
-            if (String.IsNullOrEmpty (line))
+            if (String.IsNullOrEmpty(line))
                 return null;
 
-            var tokens = line.Split (',');
+            var tokens = line.Split(',');
             if (tokens.Length != 7)
                 return null;
 
-            return new Movie () {
-                Id = Int32.TryParse (tokens[0], out var id) ? id : 0,
-                Title = tokens[1].Trim ('"', ' '),
-                Description = tokens[2].Trim ('"', ' '),
-                Rating = tokens[3].Trim ('"', ' '),
-                ReleaseYear = Int32.TryParse (tokens[4], out var year) ? year : 1900,
-                RunLength = Int32.TryParse (tokens[5], out var length) ? length : 0,
-                HasSeen = Boolean.TryParse (tokens[6], out var seen) ? seen : false,
-            };
+            return new Movie() {
+                Id = Int32.TryParse(tokens[0], out var id) ? id : 0,
+                Title = tokens[1].Trim('"', ' '),
+                Description = tokens[2].Trim('"', ' '),
+                Rating = tokens[3].Trim('"', ' '),
+                ReleaseYear = Int32.TryParse(tokens[4], out var year) ? year : 1900,
+                RunLength = Int32.TryParse(tokens[5], out var length) ? length : 0,
+                HasSeen = Boolean.TryParse(tokens[6], out var seen) ? seen : false,
+            };            
         }
 
         private string SaveMovie ( Movie item )
