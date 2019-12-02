bc58f803fc352689a7877dfb38ac1d05ab7ad34e
geoffrey.kio <geoffrey.kio@NEITD-211214>
2019-12-02 17:52:34 -0600

    552

--- classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Controllers/MovieController.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Controllers/MovieController.cs
@@ -21,11 +21,37 @@ namespace Itse1430.MovieLib.WebHost.Controllers
         [HttpGet]
         public ActionResult Index ()
         {
-            var items = _database.GetAll ();
+            var items = _database.GetAll ()
+                                 .OrderBy (x => x.Title)
+                                 .ThenBy (x => x.ReleaseYear);
 
             var model = items.Select (i => i.ToModel ());
             
             return View (model);
+        }
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            return View ();
+        }
+
+        [HttpPost]
+        public ActionResult Create ( MovieModel model)
+        {
+            //Validate
+            //Save if valid
+            try
+            {
+                //Save if valid
+                var movie = model.ToDomain ();
+                _database.Add (movie);
+                return RedirectToAction ("Index");
+            } catch (Exception e)
+            {
+                return View (model);
+
+            };
+            
         }
         private readonly IMovieDatabase _database;
     }
